5998f77efaf5609e0b118daaf3b38c0a0d4d24c6
Rafael Rocha <mouravieira44@gmail.com>
2024-08-21 11:35:44 -0300

    att trab final I

---
+++ Tomorrow JavaScript/calculadora/script.js
@@ -0,0 +1,42 @@
+function calcular() {
+    let prim = document.getElementById("primeiro-valor").value
+    let seg = document.getElementById("segundo-valor").value
+
+    prim = parseFloat(prim)
+    seg = parseFloat(seg)
+
+    let escolha = document.getElementById("opcoes-calculo")
+    
+    if (escolha.value == "somar") { // SOMA
+        let result = prim + seg
+
+        let elemento = document.getElementById("saida-resposta")
+        elemento.textContent = result 
+        
+    } else if (escolha.value == "subtrair") { // SUBTRAIR
+        let result = prim - seg
+
+        let elemento = document.getElementById("saida-resposta")
+        elemento.textContent = result 
+
+    } else if (escolha.value == "multiplicar") { // MULTIPLICAR
+        let result = prim * seg
+
+        let elemento = document.getElementById("saida-resposta")
+        elemento.textContent = result 
+
+    } else { // DIVIDIR
+        let result = prim / seg
+
+        let elemento = document.getElementById("saida-resposta")
+        elemento.textContent = result 
+    }
+}
+
+function limpar() {
+    let prim = document.getElementById("primeiro-valor")
+    let seg = document.getElementById("segundo-valor")
+
+    prim.value = ""
+    seg.value = ""
+}
